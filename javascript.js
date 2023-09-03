5027ae0721e2866dc97169bf14104362de517362
Arango155 <jdarango031@gmail.com>
2023-09-02 18:50:18 -0600

    modify

--- javascript.js
+++ javascript.js
@@ -4,3 +4,23 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "josarango024@gmail.com",
+//     Password: "",
+//     To: "josarango024@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "Contact Form",
+//     Body:
+//       "Name: " +
+//       document.getElementById("name").value +
+//       "<br> Email: " +
+//       document.getElementById("email").value +
+//       "<br> Subject: " +
+//       document.getElementById("subject").value +
+//       "<br> Message: " +
+//       document.getElementById("message").value,
+//   }).then((message) => alert(message));
+// }
